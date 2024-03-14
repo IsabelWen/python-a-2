63db3cf3b66540a16e15a83e735487979e485f7e
Isy <isy1997@freenet.de>
2024-03-14 16:55:10 +0100

    Add tests

--- A2_Recipe_App/src/recipes/models.py
+++ A2_Recipe_App/src/recipes/models.py
@@ -10,7 +10,7 @@ difficulty_choices= (
 
 class Recipe(models.Model):
     name =  models.CharField(max_length=100)
-    cooking_time = models.IntegerField
+    cooking_time = models.IntegerField(help_text='in min', default=0)
     ingredients = models.CharField(max_length=250)
     difficulty = models.CharField(max_length=12, choices=difficulty_choices, default='Easy')
 
